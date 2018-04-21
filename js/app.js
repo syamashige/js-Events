18321208baaa594d7b1c076be5bb2cf1978c6b0f
Sarah <yamashige.sarah@gmail.com>
2018-04-20 23:54:09 -1000

    Add Orea price on hover.

--- js/app.js
+++ js/app.js
@@ -73,6 +73,13 @@ function redFace () {
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+var price = 'show';
+
+function showPrice () {
+    var newPrice = document.getElementById('price');
+
+    newPrice.innerHTML = "$5.55";
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
